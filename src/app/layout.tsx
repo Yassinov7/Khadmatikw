84a85fa89b928360bbe2af7b5d446f33e7df1bcf
Mohammad-Yassine-eng <2002m.yassine@gmail.com>
2025-06-12 20:53:23 +0300

    deleted favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ar" dir="rtl">
       <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@400;700&display=swap" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1D4ED8" />
